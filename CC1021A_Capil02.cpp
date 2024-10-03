c48b5061e67fe03912d022991d8fe839cce6ec8f
Joyce <annecapil03@gmail.com>
2024-10-03 10:04:55 +0800

    Add files via upload

---
+++ CC1021A_Capil02.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+    string name, age;
+    cout << "Our Lady of Lourdes College Foundation\n";
+    cout << "College of Computer Studies\n";
+    cout << "----------------------------------------------------------------------" << endl;
+    cout << "Enter the name of the student\t|\t";
+    cin >> name;
+    cout << "Age\t\t\t\t|\t";
+    cin >> age;
+    cout << "----------------------------------------------------------------------" << endl;
+    cout << "Howdy " << name << ", your age is " << age << " and for me you are too young!" << endl;
+    cout << "----------------------------------------------------------------------" << endl;
+    return 0;
+
+}
+
+
