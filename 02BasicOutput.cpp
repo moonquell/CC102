cdb3fc7af33432d6a95ae5bcaaf7230975e1e85d
Joyce <annecapil03@gmail.com>
2024-10-03 09:25:17 +0800

    Create 02BasicOutput.cpp

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main () 
+
+{
+  cout <<  "Our lady of Lourdes College Foundation" << endl;
+  cout <<  "College of Computer Studies" << endl;
+  cout <<  "BSIT 1-A" << endl;
+  cout <<  "Joyce Anne Y. Capil" << endl;
+  cout <<  "Labo, Camarines Norte" << endl;
+
+return 0;
+
+}
