2f3b3f26d443f2a8cc8e8c8f710a657dd97d1125
Joyce <annecapil03@gmail.com>
2024-10-03 09:53:28 +0800

    Create 04BasicInput.cpp

---
+++ 04BasicInput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main () 
+
+{
+  string Nickname, Course, School; 
+  cout << "Nickname: " ;
+  cin >> Nickname;
+  cout <<  "Course-Year Level: " ;
+  cin >>  Course;
+  cout <<  "School: " ;
+  cin >>  School;
+
+  cout << "Wow Congrats" << Nickname << "!" << Course << " is a nice course. and you are studying in" << School << " which is one of the Center of Excellence in Tertiary Education.";
+  
+
+return 0;
+
