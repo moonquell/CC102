ee030828ff5e2649dcefcfd0a8dddf2a2b93ffb7
Joyce <annecapil03@gmail.com>
2024-10-15 10:12:46 +0800

    Add files via upload

---
+++ capilulit.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	float sugarPriceUSD, ricePricePound, sardinesPricePound, coffeePriceUSD, milkPriceUSD;
+	float sugarQty, riceQty, sardinesQTy, coffeeQty, milkQty;
+	float usdToPhp, poundToPhp;
+	
+	cout << "Enter sugar Quantity" ;
+	cin >> sugarQty;
+	
+	cout "Enter rice Quantity" ;
+	cin >> riceQty;
+	
+	cout << "Enter sardines Quantity" ;
+	cin >> sardinesQty;
+	
+	cout << "Enter coffee Quantity" ;
+	cin >> coffeeQty;
+	
+	cout << "Enter milk Quantity" ;
+	cin >> milkQty;
+	
+	cout << "Enter price of sugar in Php";
+	cin >> sugarPriceUSD;
+	
+	cout << "Enter price of rice in Php";
+	cin >> ricePricePound;
+	
+	cout << "Enter price of sardines in Php";
+	cin >> sardinesPriceUSD;
+	
+	cout << "Enter price of coffee in Php";
+	cin >> coffeePriceUSD;
+	
+	cout << "Enter price of milk in Php";
+	cin >> milkPriceUSD;
+	
+	cout << "totalCostPHP = sugarCostPHP + riceCOstPHP + sardinesCostPHP + coffeeCostPHP + milkCostPHP" ;
+	
+	
+	return 0;
+}
+	
