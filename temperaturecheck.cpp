766883960b780e66bd47bd1ad4ac4cd884a0df71
Joyce <annecapil03@gmail.com>
2024-11-13 09:03:47 +0800

    temperaturecheck.cpp

--- temperaturecheck.cpp
+++ temperaturecheck.cpp
@@ -5,11 +5,11 @@ int main () {
 	cout << "Enter the temperature: ";
 	cin >> temperature;
 	if(temperature<32){
-		cout << "It's cold! Bring a heavy jacket." << endl;
+		cout << "Bring a heavy jacket." << endl;
 		}else if (temperature >= 32 && temperature <= 50) {
-			cout << "It's a bit chilly. Bring a light jacket." << endl;
+			cout << "Bring a light jacket." << endl;
 			}else if (temperature > 50){
-				cout << "No need for a jacket!" << endl;
+				cout << "No need for a jacket." << endl;
 			}	
 			return 0;
 }
