d72072face154dd4845a61eaa0eb712d2888552e
Joyce <annecapil03@gmail.com>
2024-11-19 09:07:35 +0800

    Update temperaturecheck.cpp

--- temperaturecheck.cpp
+++ temperaturecheck.cpp
@@ -4,13 +4,12 @@ int main () {
 	int temperature;	
 	cout << "Enter the temperature: ";
 	cin >> temperature;
-	if (temperature<32){
-		cout << "Bring a heavy jacket." << endl;
-		}else if (temperature >= 32 && temperature <= 50){
-			cout << "Bring a light jacket." << endl;
-			}else if (temperature > 50){
-				cout << "No need for a jacket." << endl;
-			}	
-			return 0;
+	if (temperature<32) {
+		cout << "Bring a heavy jacket." << endl; }
+	else if (temperature >= 32 && temperature <= 50) {
+			cout << "Bring a light jacket." << endl; }
+	else if (temperature > 50) {
+				cout << "No need for a jacket." << endl; }	
+return 0;
 }
 
