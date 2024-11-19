38fbd44961305affb5762049d8201cd6c58d20a5
Joyce <annecapil03@gmail.com>
2024-11-19 09:04:46 +0800

    Add files via upload

---
+++ ageact.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int age;
+	cout << "Enter age: "; 
+	cin >> age;
+	if(age<16) {
+		cout << "Too young to drive."; } 
+	else if(age==16) {
+		cout << "Better clear the road."; }
+	else if(age>16) {
+		cout << "You're getting pretty old." }
+	return 0;
+}
