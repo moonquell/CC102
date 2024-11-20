d7759582a46bc57025cf94bcd57b413136f35f8d
Joyce <annecapil03@gmail.com>
2024-11-20 18:50:55 +0800

    ageopt.cpp

--- ageact.cpp
+++ ageact.cpp
@@ -2,16 +2,14 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
 	int age;
 	cout << "Enter age: "; 
 	cin >> age;
-	if(age<16) {
-		cout << "Too young to drive."; } 
-	else if(age==16) {
-		cout << "Better clear the road."; }
-	else if(age>16) {
-		cout << "You're getting pretty old." }
+	
+	if(age<16) cout << "Too young to drive."; 
+	else if(age==16) cout << "Better clear the road."; 
+	else cout << "You're getting pretty old." 
+	
 	return 0;
 }
