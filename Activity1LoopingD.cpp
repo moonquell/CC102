053731bb131e9d07063b7d7ef4bbcaad7872e54a
Joyce <annecapil03@gmail.com>
2024-11-30 21:42:28 +0800

    Add files via upload

---
+++ Activity1LoopingD.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int z = i; z > 0; z--) cout << z << " ";
+        cout << '\n';
+    }
+    return 0;
+}
