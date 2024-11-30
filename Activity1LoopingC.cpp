12846d9ec8c49961964a055a4299cf3f7481924d
Joyce <annecapil03@gmail.com>
2024-11-30 21:40:47 +0800

    Add files via upload

---
+++ Activity1LoopingC.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Row count: ";
+    cin >> n;
+
+    for (int i = n; i > 0; i--) {
+        for (int k = 0; k < n - i; k++) cout << " ";
+        for (int j = 0; j < i; j++) cout << "#";
+        cout << '\n';
+    }
+    return 0;
+}
